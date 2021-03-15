9100431521f10cbd3d7376a6a8876c1df5c1bfaf
Nir Bavariya <73009310+Nirbavariya@users.noreply.github.com>
2021-03-15 22:58:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var ran1 = Math.floor(Math.random()*6)+1;
+var loc = "images/dice" + ran1 + ".png" ;
+document.querySelectorAll("img")[0].setAttribute("src", loc);
+
+var ran2 = Math.floor(Math.random()*6)+1;
+var loc1 = "images/dice" + ran2 + ".png" ;
+document.querySelectorAll("img")[1].setAttribute("src", loc1);
+
+if(ran1>ran2)
+{
+    document.querySelector("h1").innerHTML="🚩Player 1 won!!! ";
+}
+if(ran1<ran2)
+{
+    document.querySelector("h1").innerHTML="Player 2 won🚩!!! ";
+}
+if(ran1===ran2)
+{
+    document.querySelector("h1").innerHTML="It was a tie!!! ";
+}
