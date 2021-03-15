34341fef736b3b57ec5d801757987fba0abd8750
Nir Bavariya <73009310+Nirbavariya@users.noreply.github.com>
2021-03-15 23:00:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var ran1 = Math.floor(Math.random()*6)+1;
-var loc = "images/dice" + ran1 + ".png" ;
+var loc = "dice" + ran1 + ".png" ;
 document.querySelectorAll("img")[0].setAttribute("src", loc);
 
 var ran2 = Math.floor(Math.random()*6)+1;
-var loc1 = "images/dice" + ran2 + ".png" ;
+var loc1 = "dice" + ran2 + ".png" ;
 document.querySelectorAll("img")[1].setAttribute("src", loc1);
 
 if(ran1>ran2)
